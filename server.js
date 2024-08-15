677b6dcc2ee207c6988f10e05503d7eede94d963
GabrielPIAUI <gabriel.r.machado6@aluno.senai.br>
2024-08-15 10:51:17 -0300

    fotografiaGelada pt.1

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+dotenv.config();
+
+const app = express();
